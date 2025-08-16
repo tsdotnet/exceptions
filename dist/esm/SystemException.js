577f9f363c6ffb701b75241102e13f5d4c7cb403
electricessence <5899455+electricessence@users.noreply.github.com>
2025-08-16 01:34:44 -0700

    fix: Generate proper ESM modules instead of CommonJS
    - Update tsconfig.esm.json to use module: esnext and moduleResolution: bundler
    - ESM build now generates true ES modules with import/export syntax
    - Fixes violation where ESM build was generating CommonJS code with require/exports

--- dist/esm/SystemException.js
+++ dist/esm/SystemException.js
@@ -1,17 +1,13 @@
-"use strict";
 /*!
  * @author electricessence / https://github.com/electricessence/
  * Licensing: MIT
  * Based upon: https://msdn.microsoft.com/en-us/library/system.systemexception%28v=vs.110%29.aspx
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const Exception_1 = tslib_1.__importDefault(require("./Exception"));
+import Exception from './Exception';
 const NAME = 'SystemException';
-class SystemException extends Exception_1.default {
+export default class SystemException extends Exception {
     getName() {
         return NAME;
     }
 }
-exports.default = SystemException;
 //# sourceMappingURL=SystemException.js.map
