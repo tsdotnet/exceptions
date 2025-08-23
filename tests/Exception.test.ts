5a8df1ae4544a0b6feca18b66972904f7383f188
electricessence <5899455+electricessence@users.noreply.github.com>
2025-08-23 09:44:46 -0700

    Dev update.

--- tests/Exception.test.ts
+++ tests/Exception.test.ts
@@ -1,5 +1,5 @@
 import {describe, it, expect} from 'vitest';
-import { ArgumentException, Exception } from '../src/index.ts';
+import { ArgumentException, Exception } from '../src/index';
 
 const message = 'Test';
 
